87061a7e092d2fb26f697e49edbb662119588263
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2017-11-04 14:28:33 +0100

    Added InitBinder To Trim Whitespaces
    Added InitBinder To Trim Whitespaces for every webrequest. Preprocesses the String form data.

--- src/main/java/nl/thuis/tutorial/controller/CustomerController.java
+++ src/main/java/nl/thuis/tutorial/controller/CustomerController.java
@@ -2,9 +2,12 @@ package nl.thuis.tutorial.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +16,14 @@ import nl.thuis.tutorial.beans.Customer;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// This method is called for every webrequest. Preprocess every String form data
+	// Trims whitespaces in the String-objects
+	@InitBinder
+	public void initBinderTrimStrings(WebDataBinder binder) {
+		StringTrimmerEditor trimmer = new StringTrimmerEditor(true);
+		binder.registerCustomEditor(String.class, trimmer);
+	}
 
 	@RequestMapping("/showform")
 	public String showform(Model model) {
